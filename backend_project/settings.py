d9e1d7789bd986f4abf8d74d5a31fd5db6700ffc
josiasdev <francisco.batista67@alu.ufc.br>
2025-10-02 22:58:05 -0300

    feat: Formatar em json.

--- backend_project/settings.py
+++ backend_project/settings.py
@@ -117,6 +117,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 REST_FRAMEWORK = {
+    
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ],
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_AUTHENTICATION_CLASSES': [],
 }
