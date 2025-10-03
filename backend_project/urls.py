5ea16a21444ca308ffd29f12bfb195e6c3d2e289
josiasdev <francisco.batista67@alu.ufc.br>
2025-10-03 00:35:13 -0300

    feat: link de redirecionamento

--- backend_project/urls.py
+++ backend_project/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path('swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
     path('api/internal-schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('', RedirectView.as_view(url='/swagger-ui/', permanent=False)),
+    path('', RedirectView.as_view(url='swagger-ui/', permanent=False)),
 ]
