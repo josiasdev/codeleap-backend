fb1aa0caa3948b5776eb044fdf67fe09d9fed091
josiasdev <francisco.batista67@alu.ufc.br>
2025-10-02 22:47:09 -0300

    feat: Desabilitado a autenticação.

--- backend_project/settings.py
+++ backend_project/settings.py
@@ -118,4 +118,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_AUTHENTICATION_CLASSES': [],
 }
