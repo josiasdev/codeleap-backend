7ac68c3545ac8eface3486419c7017784d095fa2
josiasdev <francisco.batista67@alu.ufc.br>
2025-10-03 00:32:02 -0300

    feat: Corrigindo redirecionamento

--- backend_project/urls.py
+++ backend_project/urls.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+from django.views.generic import RedirectView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('careers.urls')),
-
-    path('api/internal-schema/', SpectacularAPIView.as_view(), name='schema'),
     path('swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path('api/internal-schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('', RedirectView.as_view(url='/swagger-ui/', permanent=False)),
 ]
